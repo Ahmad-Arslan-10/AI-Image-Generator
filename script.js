6f026e6e80f9ed77631ddc9adc72f4ca500d809d
Ahmad-Arslan-10 <arslanimtiaz849@gmail.com>
2025-03-12 20:18:56 +0500

    Updated JS File

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ const modelSelect = document.querySelector("model-select");
 const countSelect = document.querySelector("count-select");
 const ratioSelect = document.querySelector("ratio-select");
 const gridGallery = document.querySelector(".gallery-grid");
-
+const API_KEY = "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";
+  
 const examplePrompts = [
-      "A magic forest with glowing plants and fairy homes among giant mushrooms",
+  "A magic forest with glowing plants and fairy homes among giant mushrooms",
   "An old steampunk airship floating through golden clouds at sunset",
   "A future Mars colony with glass domes and gardens against red mountains",
   "A dragon sleeping on gold coins in a crystal cave",
@@ -26,7 +27,7 @@ const examplePrompts = [
 ];
 
 (() => {
-  const savedTheme =localStorage.getItem("theme");
+  const savedTheme = localStorage.getItem("theme");
   const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
   const isDarkTheme = savedTheme === "dark" || (!savedTheme && systemPrefersDark);
@@ -40,12 +41,49 @@ const toggleTheme = () => {
   themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
 };
 
+const getImageDimensions = (aspectRatio, baseSize = 512) => {
+ const [width,height] = aspectRatio.split("/").map(Number);
+ const scaleFactor = baseSize / Math.sqrt(width*height);
+
+ let calculatedWidth = Math.round(width*scaleFactor);
+ let calculatedHeight = Math.round(height*scaleFactor);
+
+ calculatedWidth = Math.floor(calculatedWidth/16)*16;
+ calculatedHeight = Math.floor(calculatedHeight/16)*16;
+
+ return {width: calculatedWidth, height:calculatedHeight};
+}
+
+const generateImages = async (selectedModel,imageCount,aspectRatio,promptText) => {
+ const MODEL_URL = `https://router.huggingface.co/hf-inference/models/${selectedModel}`;
+ getImageDimensions(aspectRatio);
+
+ try{
+  const response = await fetch(MODEL_URL,{
+			headers: {
+				Authorization: `Bearer ${API_KEY}`,
+				"Content-Type": "application/json",
+			},
+			method: "POST",
+			body: JSON.stringify({
+        inputs: promptText,
+        parameters: {width,height},
+        options: {wait_for_model: true, user_cache: false},
+      }),
+		});
+
+	const result = await response.blob();
+ } catch(error){
+
+  console.log(error);
+}
+};
+
 const createImageCards = (selectedModel,imageCount,aspectRatio,promptText) => {
   gridGallery.innerHTML="";
   
   for (let i=0;i<imageCount;i++){
-       gridGallery.innerHTML += `<div class = "img-card loading" id="img-card-${i}" style="aspect-ratio: $
-  {aspectRatio}">
+       gridGallery.innerHTML += `<div class="img-card loading" id="img-card-${i}" style="aspect-ratio: ${aspectRatio}">
             <div class="status-container">
               <div class="spinner"></div>
               <i class="fa-solid fa-triangle-exclamation"></i>
@@ -54,6 +92,7 @@ const createImageCards = (selectedModel,imageCount,aspectRatio,promptText) => {
               <img src="test.png" class="result-img">               
           </div>`;
   }
+  generateImages(selectedModel,imageCount,aspectRatio,promptText);
 };
 
 
@@ -68,7 +107,6 @@ const handleFormSubmit = (e) => {
  createImageCards(selectedModel,imageCount,aspectRatio,promptText);
 
 }
-
 
 promptBtn.addEventListener("click", () => {
    const prompt = examplePrompts[Math.floor(Math.random() * examplePrompts.length)];
